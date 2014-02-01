c2567c7e7c9b9bf0ca1132334ca86f91f7cf82d7
parepallykiran <saikiran@saikiran-PC>
2014-02-01 11:54:22 -0800

    Final commit (XML)

--- cloudwick/src/com/cloudwick/domxml/DomExample.java
+++ cloudwick/src/com/cloudwick/domxml/DomExample.java
@@ -1,19 +1,26 @@
 package com.cloudwick.domxml;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class DomExample {
+	private Document doc;
+	private List<Employee> emplist = new ArrayList<Employee>();
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) throws ParserConfigurationException,
+			SAXException, IOException {
 		DomExample obj = new DomExample();
 		obj.parseXmlFile("Employees.xml");
+		obj.parseDocument();
+		obj.printData();
 
 	}
 
@@ -21,7 +28,49 @@ public class DomExample {
 			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document doc = builder.parse(filename);
+		doc = builder.parse(filename);
 	}
 
+	public void parseDocument() {
+		Element rootel = doc.getDocumentElement();
+		NodeList empnodelist = rootel.getElementsByTagName("Employee");
+		for (int i = 0; i < empnodelist.getLength(); i++) {
+			Element emp = (Element) empnodelist.item(i);
+			Employee empobj = getEmployee(emp);
+			emplist.add(empobj);
+		}
+	}
+
+	public Employee getEmployee(Element emp) {
+		Employee empobj = new Employee();
+		String type = emp.getAttribute("type");
+
+		NodeList idlist = emp.getElementsByTagName("id");
+		Element idElem = (Element) idlist.item(0);
+		int id = Integer.parseInt(idElem.getFirstChild().getNodeValue());
+
+		NodeList agelist = emp.getElementsByTagName("age");
+		Element ageElem = (Element) agelist.item(0);
+		int age = Integer.parseInt(ageElem.getFirstChild().getNodeValue());
+
+		NodeList namelist = emp.getElementsByTagName("name");
+		Element nameElem = (Element) namelist.item(0);
+		String name = nameElem.getFirstChild().getNodeValue();
+
+		empobj.setAge(age);
+		empobj.setId(id);
+		empobj.setName(name);
+
+		return empobj;
+
+	}
+
+	public void printData() {
+		for (Employee emp : emplist) {
+			System.out.println(emp.getId());
+			System.out.println(emp.getName());
+			System.out.println(emp.getAge());
+			System.out.println(emp.getType());
+		}
+	}
 }
--- cloudwick/src/com/cloudwick/domxml/Employee.java
+++ cloudwick/src/com/cloudwick/domxml/Employee.java
@@ -7,42 +7,34 @@ public class Employee {
 	private int age;
 	private String name;
 
-	
 	public String getType() {
 		return type;
 	}
 
-	
 	public void setType(String type) {
 		this.type = type;
 	}
 
-	
 	public int getId() {
 		return id;
 	}
 
-	
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	
 	public int getAge() {
 		return age;
 	}
 
-	
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-	
 	public String getName() {
 		return name;
 	}
 
-	
 	public void setName(String name) {
 		this.name = name;
 	}
